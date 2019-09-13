ad915092a367a3a8102588e995365bb5c873a752
wengzi <Hush.TianLLei@outlook.com>
2019-09-13 15:19:44 +0800

    init commit

---
+++ src/main/java/cn/bdqn/exam/server/impl/AnswerServiceImpl.java
@@ -0,0 +1,8 @@
+package cn.bdqn.exam.server.impl;
+
+import cn.bdqn.exam.server.AnswerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerServiceImpl implements AnswerService {
+}
